6986f88d3eb0b2df4731a2473537c36159186dc9
Lonely0710 <205923907@qq.com>
2025-07-23 17:30:35 +0800

    change subtitle

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const themeConfig: ThemeConfig = {
     // site title
     title: 'Lonely\'s Blog',
     // site subtitle
-    subtitle: '命は続く，日々のゲームは続く',
+    subtitle: '命は続く 日々のゲームは続く',
     // site description
     description: 'Retypeset is a static blog theme based on the Astro framework. Inspired by Typography, Retypeset establishes a new visual standard and reimagines the layout of all pages, creating a reading experience reminiscent of paper books, reviving the beauty of typography. Details in every sight, elegance in every space.',
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
